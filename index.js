b47c42218177a2636d623b0f904ed9237cbd6051
Alyssa Cucunato <acucunato18@gmail.com>
2020-01-27 14:11:38 -0500

    added axios, and inquirer call

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+
+const questions = [];
+
+function writeToFile(fileName, data) {}
+
+function init() {}
+
+init();
